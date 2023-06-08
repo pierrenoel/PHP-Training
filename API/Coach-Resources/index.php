cee9d49dfda278d57b1bdc62b7ed5fe9cab5899c
pierrenoel <pierrenoel1509@gmail.com>
2023-06-08 16:02:32 +0200

    update

--- API/Coach-Resources/index.php
+++ API/Coach-Resources/index.php
@@ -12,31 +12,12 @@ $router->get('/post/:id', function($id) {
     (new \app\controllers\PostController())->show($id);
 });
 
+$router->post('/post/add',function(){
+    (new \app\controllers\PostController())->create($_POST);
+});
+
+$router->delete('/post/delete/:id',function ($id){
+    (new \app\controllers\PostController())->destroy($id);
+});
 
 $router->run();
-
-/*
-
-$uri =  explode('/',$_SERVER['REQUEST_URI']);
-$request_method = $_SERVER['REQUEST_METHOD'];
-
-// /posts
-if($uri[1] == 'posts')
-    if($request_method == "GET") (new \app\controllers\PostController())->index();
-
-
-// post/1
-if($uri[1] == 'post' && !empty($uri[2]))
-    if($request_method == "GET") (new \app\controllers\PostController())->show($uri[2]);
-
-if($uri[1] == 'add-post')
-{
-    if($request_method == "POST") (new \app\controllers\PostController())->create($_POST);
-}
-
-if($uri[1] == 'delete' && !empty($uri[2]))
-{
-    if($request_method == "GET") (new \app\controllers\PostController())->destroy($uri[2]);
-}
-
-*/
